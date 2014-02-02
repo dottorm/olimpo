75e64430d26d311697f51a1cac767aa1d0180159
dottorm <marcoparrillo@gmail.com>
2014-02-02 18:53:51 +0100

    added Plugin in Project

--- app/com/olimpo/models/Project.java
+++ app/com/olimpo/models/Project.java
@@ -1,8 +1,11 @@
 package com.olimpo.models;
 
+import java.util.List;
+
 public class Project {
 	
 	private String name;
+	private List<Plugin> plugins;
 	
 	public Project(String name){
 		this.name = name;
@@ -12,8 +15,16 @@ public class Project {
 		this.name = name;
 	}
 	
+	public void addPlugin(Plugin plugin){
+		plugins.add(plugin);
+	}
+	
 	public String getName(){
 		return this.name;
+	}
+	
+	public List<Plugin> allPlugins(){
+		return this.plugins;
 	}
 	
 	public String toString(){
